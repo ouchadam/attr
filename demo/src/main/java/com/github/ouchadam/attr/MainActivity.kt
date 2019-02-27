def6a99c7553f0f563b23f6435e07c57828ea823
Adam Brown <adampsbrown@gmail.com>
2019-02-27 08:25:19 +0000

    simplifying the demo

--- demo/src/main/java/com/github/ouchadam/attr/MainActivity.kt
+++ demo/src/main/java/com/github/ouchadam/attr/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.github.ouchadam.attr
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.PaintDrawable
+import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
+import android.util.AttributeSet
+import android.widget.Toast
 import androidx.annotation.ColorInt
 import androidx.annotation.Dimension
 import androidx.annotation.Px
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatTextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,19 +21,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val shape = theme.attr<Shape>()
+        val themeAttributes = theme.attr<ThemeAttributes>()
 
-
-        button.setOnClickListener {  }
+        customView.setTextColor(themeAttributes.colorPrimary)
+        customView.setOnClickListener {
+            Toast.makeText(this, "is light theme: ${themeAttributes.isLightTheme}", Toast.LENGTH_SHORT).show()
+        }
     }
-
 }
 
 @Attr
-data class Shape(
+data class ThemeAttributes(
     @Attr.Id(R.attr.colorPrimary) @ColorInt val colorPrimary: Int,
-    @Attr.Id(R.attr.isLightTheme) val isLightTheme: Boolean,
-    @Attr.Id(R.attr.dividerPadding) @Dimension val dividerPaddingDp: Float,
-    @Attr.Id(R.attr.dividerPadding) @Px val dividerPaddingPx: Int,
-    @Attr.Id(R.attr.layout_constraintCircleAngle) val toolTipAnimTime: Int
+    @Attr.Id(R.attr.isLightTheme) val isLightTheme: Boolean
 )
+
+class CustomView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int = 0
+) : AppCompatTextView(context, attrs, defStyleAttr) {
+
+    init {
+        val customAttributes = context.theme.attr<CustomAttributes>(attrs)
+        val baseBackground = PaintDrawable(customAttributes.backgroundColor).apply {
+            setCornerRadius(customAttributes.radius)
+        }
+        val rippleMask = PaintDrawable(Color.BLACK).apply { setCornerRadius(customAttributes.radius) }
+        background = RippleDrawable(ColorStateList.valueOf(customAttributes.rippleColor), baseBackground, rippleMask)
+    }
+
+    @Attr
+    data class CustomAttributes(
+        @Attr.Id(R.attr.radius) @Dimension val radius: Float,
+        @Attr.Id(R.attr.rippleColor) @ColorInt val rippleColor: Int,
+        @Attr.Id(R.attr.backgroundColor) @ColorInt val backgroundColor: Int
+    )
+}
